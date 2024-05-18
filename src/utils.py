b86dcc9062c568146fec82aa35ff602841762e5d
lucapl <lukasz.andryszewski@outlook.com>
2024-05-18 21:17:38 +0200

    Moved row substractions and filtering from report

--- src/utils.py
+++ src/utils.py
@@ -22,3 +22,15 @@ def undersample(X,y,samples=None,class_0=0):
     np.random.shuffle(new_data)
 
     return new_data[:,:-1],new_data[:,-1]
+
+def calculate_2d_differences(array:np.array):
+    difs = array[:,np.newaxis,:] - array 
+    return difs.reshape(-1,array.shape[1])
+
+def calculate_1d_differences(vector:np.array):
+    difs = vector[:,np.newaxis] - vector
+    return difs.reshape(-1)
+
+def filter_data(X,y):
+    sel = (y != 0)
+    return X[sel],y[sel]
